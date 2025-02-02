4146ef47d5cdd07b46fac39207fa1fdb269d3557
WSQ.coder <WSQ.coder@gmail.com>
2025-02-02 18:48:51 +0300

    add first config

---
+++ Docker-Image/Apache_php/index.php
@@ -0,0 +1,10 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "<h2>Created by <font color=orange>Vasiliy Rylov</font></h2>";
+echo "<br><p>Server IP Address is: $ip_server</p>";
+
+?>
+
+
